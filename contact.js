40def3b863f25f91353576f42b8fb7aa7591daee
Shashi <Shashikant@leewayhertz.com>
2021-05-06 14:24:11 +0530

    commit file

--- contact.js
+++ contact.js
@@ -9,9 +9,9 @@
 <p id="demo"></p>
 
 <script>
-    var x, y, z;             // Declare 3 variables
-    x = 5;                   // Assign the value 5 to x
-    y = 6;                    // Assign the value 6 to y
+     var x, y, z;             // Declare 3 variables
+     x = 5;                   // Assign the value 5 to x
+     y = 6;                    // Assign the value 6 to y
     z = x+y;                 // Assign the sum of x and y to z
 
    document.getElementById("demo").innerHTML =
