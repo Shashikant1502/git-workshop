9d9aabda7277438891d275d4cd418810b30feb97
Shashi <Shashikant@leewayhertz.com>
2021-05-06 11:11:34 +0530

    added more js

---
+++ through.js
@@ -0,0 +1,17 @@
+!< DOCTYPE html >
+<html>
+    <body>
+        <h2>JavaScript<h2>
+            <p>A<B> JavaScript</B></p>program<b></b>AbortSignal</b>
+
+            <Script>
+            <p id="demo"></p>
+
+                var a,b,c;
+                a =3;
+                b =4;
+                c =a=b;
+            document.getElementById(demo).innerHTML= value
+            </Script>
+        </body>
+<html>
