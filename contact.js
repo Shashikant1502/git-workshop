48c28e312b53e0698a925bd2cf97f0396f2844e0
Shashi <Shashikant@leewayhertz.com>
2021-05-06 15:15:44 +0530

    adding extra space

--- contact.js
+++ contact.js
@@ -1,4 +1,5 @@
 <script>  
-emp={id:102,name:"Shashi",salary:40000}  
-document.write(emp.id+" "+emp.name+" "+emp.salary);  
-</script>  
+    emp={id:102,name:"Shashi",salary:40000}  
+
+    document.write(emp.id+" "+emp.name+" "+emp.salary);  
+</script> 
